eef34014b1e9a9fede0ef395e2c3d8abef36e68f
Golusu Hemanth Kumar <hemanthkgolusu@gmail.com>
2025-09-19 00:43:26 -0400

    Initial Commit

---
+++ I Am Poor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I Am Poor
+//
+//  Created by HEMANTH KUMAR GOLUSU on 9/19/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
